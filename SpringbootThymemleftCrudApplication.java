f0d0b0634fbf495976c5f9e2c065778bfadf564e
r2raviprasad <69834666+r2raviprasad@users.noreply.github.com>
2022-07-11 19:52:18 +0530

    Add files via upload

---
+++ SpringbootThymemleftCrudApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootThymemleftCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootThymemleftCrudApplication.class, args);
+	}
+
+}
